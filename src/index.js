8b9de9473850530adba526cca551cebe32760bd3
Paul Campbell <paul@rslw.com>
2018-07-30 23:02:57 +0100

    An initial commit just to get things going

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+// let turingWidget = async function() {
+//   var element = document.createElement('div');
+
+//   console.log("hello!")
+
+//   // Lodash, currently included via a script, is required for this line to work
+//   const httpResponse = await axios.get("http://localhost:9394/responses/3d730140-a0b6-4019-88e7-4663160457ef")
+//   let response = httpResponse.data
+//   element.innerHTML = `Hello ${response.name}`
+
+//   document.body.appendChild(element);
+// }
+
+export default {
+  paul: "that"
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  }
+};
