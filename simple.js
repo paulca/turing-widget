bd8c326f3bd8bd6f79a46a5a79bdeda8fe9cf6e4
Paul Campbell <paul@rslw.com>
2018-08-01 12:54:57 +0100

    Add a very simple example
    This is just a pure JS example of populating a div with a form,
    posting it (via the `fetch` API) and updating the view.

---
+++ simple.js
@@ -0,0 +1,62 @@
+(function() {
+  var form = document.createElement("form");
+  var nameLabel = document.createElement("label");
+  nameLabel.innerText = "Name";
+
+  var nameInput = document.createElement("input");
+  nameLabel.appendChild(nameInput);
+
+  form.appendChild(nameLabel);
+
+  var emailLabel = document.createElement("label");
+  emailLabel.innerText = "Email";
+
+  var emailInput = document.createElement("input");
+  emailLabel.appendChild(emailInput);
+
+  form.appendChild(emailLabel);
+
+  var commentsLabel = document.createElement("label");
+  commentsLabel.innerText = "Comments";
+
+  var commentsTextarea = document.createElement("textarea");
+  commentsLabel.appendChild(commentsTextarea);
+
+  form.appendChild(commentsLabel);
+
+  var submit = document.createElement("input");
+  submit.type = "submit";
+  submit.value = "Submit";
+  form.appendChild(submit);
+
+  form.onsubmit = async function(event) {
+    event.preventDefault();
+    var response;
+    var response = {
+      name: nameInput.value,
+      email: emailInput.value,
+      comments: commentsTextarea.value
+    };
+    let rawResponse = await fetch("http://localhost:7045/responses", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ response: response, other: "something" })
+    });
+    const content = await rawResponse.json();
+    console.log(content);
+    form.remove();
+    var response = document.createTextNode(
+      `Thanks ${content.response.name}. Your response has been saved with id ${
+        content.id
+      }.`
+    );
+    element.appendChild(response);
+  };
+
+  var element;
+  element = document.getElementById("turing-widget-simple");
+  element.appendChild(form);
+})();
